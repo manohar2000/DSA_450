085ac1fbc9752bd1119e19a79d8e30efd744049d
manohar <maohar502@gmail.com>
2021-02-26 16:30:49 +0530

    reverse string

---
+++ Strings/ReverseString.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void reverseString(vector<char>& s) 
+{
+    int n = s.size(); //size of the array
+    
+    for(int i=0,j=n-1; i<=n/2,j>=n/2;i++,j--)
+    {            
+        char temp = s[i];
+        s[i] = s[j];
+        s[j] = temp;
+    }
+    
+    for(int i = 0;i<n;i++)
+    {
+        cout<<s[i]<<",";
+    }
+    
+}
+
+int main()
+{
+
+    vector<char> s = {'a','b','c','d'};
+    reverseString(s);
+}
